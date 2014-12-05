ae59a3fe5bcbd38aa3c13b1ece981b773852a986
Minsuk Lee <ykhl1itj@gmail.com>
2014-12-05 18:48:51 +0900

    refactor FSM, Load data, beautify code

--- FSM_Linux/fsm.c
+++ FSM_Linux/fsm.c
@@ -6,57 +6,21 @@
 // Copyright (c) 2014. Minsuk Lee All rights reserved.
 // see LICENSE
 
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/errno.h>
-#include <termios.h>
-
 #include "util.h"
 
-#define CONNECT_TIMEOUT 10
-
-int kbhit(void)
-{
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-    
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-    
-    ch = getchar();
-    
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-    
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-    return 0;
-}
-
-
-//#define TEST_FSM
+#define CONNECT_TIMEOUT 5
 
 #define NUM_STATE   3
 #define NUM_EVENT   8
 
-enum pakcet_type { F_CON = 0, F_ACK = 2, F_FIN = 1, F_DATA = 3 };       // Packet Type
+enum pakcet_type { F_CON = 0, F_FIN = 1, F_ACK = 2, F_DATA = 3 };   // Packet Type
 enum proto_state { wait_CON = 0, CON_sent = 1, CONNECTED = 2 };     // States
 
 // Events
 enum proto_event { RCV_CON = 0, RCV_FIN = 1, RCV_ACK = 2, RCV_DATA = 3,
-    CONNECT = 4, CLOSE = 5, SEND = 6, TIMEOUT = 7 };
+                   CONNECT = 4, CLOSE = 5,   SEND = 6,    TIMEOUT = 7 };
 
-char *pkt_name[] = { "F_CON", "F_ACK", "F_FIN", "F_DATA" };
+char *pkt_name[] = { "F_CON", "F_FIN", "F_ACK", "F_DATA" };
 char *st_name[] =  { "wait_CON", "CON_sent", "CONNECTED" };
 char *ev_name[] =  { "RCV_CON", "RCV_FIN", "RCV_ACK", "RCV_DATA",
                      "CONNECT", "CLOSE",   "SEND",    "TIMEOUT"   };
@@ -110,24 +74,20 @@ void set_timer(int sec)
 }
 void send_packet(int flag, void *p, int size)
 {
-    struct packet buf;
+    struct packet pkt;
     printf("SEND %s\n", pkt_name[flag]);
     
-    buf.type = flag;
-    if (size) {
-        buf.size = size;
-        memcpy(buf.data, p, (size > MAX_DATA_SIZE) ? MAX_DATA_SIZE : size);
-        Send((char*)&buf, size + sizeof(unsigned short) * 2);
-    } else {
-        Send((char*)&buf.type, sizeof(unsigned short));
-    }
+    pkt.type = flag;
+    pkt.size = size;
+    if (size)
+        memcpy(pkt.data, ((struct p_event *)p)->packet.data, (size > MAX_DATA_SIZE) ? MAX_DATA_SIZE : size);
+    Send((char *)&pkt, sizeof(struct packet) - MAX_DATA_SIZE + size);
 }
 
 static void report_connect(void *p)
 {
-    printf("Connected\n");
     set_timer(0);           // Stop Timer
-    // Report connection made to upper layer
+    printf("Connected\n");
 }
 
 static void passive_con(void *p)
@@ -144,27 +104,27 @@ static void active_con(void *p)
 
 static void close_con(void *p)
 {
-    printf("Connection Closed\n");
     send_packet(F_FIN, NULL, 0);
-    // Report Connected Closed to upper layer
+    printf("Connection Closed\n");
 }
 
 static void send_data(void *p)
 {
-    printf("Send Data to peer size:%d\n", ((struct p_event*)p)->size);
-    send_packet(F_DATA, &((struct p_event *)p)->packet, ((struct p_event *)p)->size);
+    printf("Send Data to peer '%s' size:%d\n",
+        ((struct p_event*)p)->packet.data, ((struct p_event*)p)->size);
+    send_packet(F_DATA, (struct p_event *)p, ((struct p_event *)p)->size);
 }
 
 static void report_data(void *p)
 {
-    printf("Data Arrived size:%d\n", ((struct p_event*)p)->size);
-    // Queue received data for upper layer user
+    printf("Data Arrived data='%s' size:%d\n",
+        ((struct p_event*)p)->packet.data, ((struct p_event*)p)->packet.size);
 }
 
 struct state_action p_FSM[NUM_STATE][NUM_EVENT] = {
   //  for each event:
-  //  RCV_CON,                    RCV_FIN,                 RCV_ACK,                       RCV_DATA,
-  //  CONNECT,                    CLOSE,                   SEND,                          TIMEOUT
+  //  RCV_CON,                 RCV_FIN,                 RCV_ACK,                       RCV_DATA,
+  //  CONNECT,                 CLOSE,                   SEND,                          TIMEOUT
 
   // - wait_CON state
   {{ passive_con, CONNECTED }, { NULL, wait_CON },      { NULL, wait_CON },            { NULL, wait_CON },
@@ -179,6 +139,8 @@ struct state_action p_FSM[NUM_STATE][NUM_EVENT] = {
    { NULL, CONNECTED },        { close_con, wait_CON }, { send_data, CONNECTED },      { NULL,        CONNECTED }},
 };
 
+int data_count = 0;
+
 struct p_event *get_event(void)
 {
     static struct p_event event;    // not thread-safe
@@ -190,22 +152,24 @@ loop:
         if(timedout) {
             timedout = 0;
             event.event = TIMEOUT;
-            goto got_it;
         } else {
             // Check Packet arrival by event_wait()
-            ssize_t n = Recv((char*)&event.packet, MAX_DATA_SIZE);
+            ssize_t n = Recv((char*)&event.packet, sizeof(struct packet));
             if (n > 0) {
                 // if then, decode header to make event
                 switch (event.packet.type) {
                     case F_CON:  event.event = RCV_CON;  break;
                     case F_ACK:  event.event = RCV_ACK;  break;
                     case F_FIN:  event.event = RCV_FIN;  break;
-                    case F_DATA: event.event = RCV_DATA; break;
+                    case F_DATA:
+                        event.event = RCV_DATA; break;
+                        event.size = event.packet.size;
+                        break;
                     default:
                         goto loop;
                 }
-                goto got_it;
-            }
+            } else
+                goto loop;
         }
     } else {
         int n = getchar();
@@ -214,17 +178,14 @@ loop:
             case '1': event.event = CLOSE;   break;
             case '2':
                 event.event = SEND;
-                event.size = 0;
+                sprintf(event.packet.data, "%09d", data_count++);
+                event.size = strlen(event.packet.data) + 1;
                 break;
             case '3': return NULL;  // QUIT
             default:
                 goto loop;
         }
-        goto got_it;
     }
-    goto loop;
-    
-got_it:
     return &event;
 }
 
@@ -270,8 +231,8 @@ main(int argc, char *argv[])
     else if (rateOfPacketLoss > 100)
         rateOfPacketLoss = 100;
         
-    
-    // SIMULATOR_INITIALIZE
+    // Login to SIMULATOR
+
     if (Login(channel, id, rateOfPacketLoss) == -1) {
         printf("Login Failed\n");
         return -1;
